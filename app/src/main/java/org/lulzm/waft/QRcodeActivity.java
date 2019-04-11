f12b5a737ae8537c0336641df77fed5f2ec5f32e
Lulz_M <yunmo159789@gmail.com>
2019-04-11 13:14:48 +0900

    QR code setting

---
+++ app/src/main/java/org/lulzm/waft/QRcodeActivity.java
@@ -0,0 +1,15 @@
+package org.lulzm.waft;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QRcodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode);
+
+
+    }
+}
