77c85b50e20acb774280a45bce15505ac1e209db
Lulz_M <yunmo159789@gmail.com>
2019-04-17 14:11:28 +0900

    Main TODO 삭제

--- app/src/main/java/org/lulzm/waft/Mainboard.java
+++ app/src/main/java/org/lulzm/waft/Mainboard.java
@@ -3,14 +3,13 @@ package org.lulzm.waft;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import androidx.fragment.app.Fragment;
 
 
 /**
@@ -25,12 +24,10 @@ public class Mainboard extends Fragment {
 
     Button bt_qr,bt_nav,bt_money,bt_chat,bt_sos1,bt_sos2;
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -48,7 +45,6 @@ public class Mainboard extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment Mainboard.
      */
-    // TODO: Rename and change types and number of parameters
     public static Mainboard newInstance(String param1, String param2) {
         Mainboard fragment = new Mainboard();
         Bundle args = new Bundle();
@@ -95,7 +91,6 @@ public class Mainboard extends Fragment {
         return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -124,7 +119,6 @@ public class Mainboard extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
